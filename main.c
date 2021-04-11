b9b52be718f8209fb0a11827371df13bba2a137a
TomHaels <73300420+TomHaels@users.noreply.github.com>
2021-04-11 19:36:00 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,24 +1,79 @@
 #include <stdio.h>
 #include <zmq.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <assert.h>
 
-void * context; //Global context, because you only need one !
+void punt(int);
+void punt (int value)
+{
+
+
+
+}
 
 int main( int argc, char * argv[] )
 {
-    printf("Hello World!\n");
-    context = zmq_ctx_new();
+        void *context = zmq_ctx_new();
+        void *publisher = zmq_socket(context, ZMQ_PUSH);
+        void *subscriber = zmq_socket (context, ZMQ_SUB);
+        const char answ[] = "startzeeslag";
+        char ask[] ="startzeeslag";
 
-    void * pusher = zmq_socket( context, ZMQ_PUSH );
-    zmq_connect( pusher, "tcp://benternet.pxl-ea-ict.be:24041" );
+        char buf [500];
+        zmq_msg_t msg;
 
-    zmq_send( pusher, "Hello World", 11, 0 );
+        //connect
+        int rc = zmq_connect( subscriber, "tcp://benternet.pxl-ea-ict.be:24042" );
+        int rp = zmq_connect(publisher, "tcp://benternet.pxl-ea-ict.be:24041");
 
-    //ALL MAGIC HAPPENS HERE
-    printf("Magic!\n");
 
-    zmq_close( pusher );
-    zmq_ctx_shutdown( context ); //optional for cleaning lady order (get ready you l*zy f*ck)
-    zmq_ctx_term( context ); //cleaning lady goes to work
-    printf("KTNXBYE!\n");
-    return 0;
+        for(int i=0; i < 5; i++)
+        {
+            rc = zmq_msg_init (&msg);
+            assert (rc == 0);
+
+
+            rc = zmq_setsockopt (subscriber, ZMQ_SUBSCRIBE, answ, strlen (answ));
+
+            rp = zmq_send(publisher, ask, sizeof(ask), 0);
+            printf("ask send\n");
+            assert (rp != -1 );//check send
+            printf("searching for players");
+            int flag =1;
+            while(flag == 1)
+            {
+                rc = zmq_recv (subscriber, buf, sizeof(buf), 0);
+                assert(rc != -1);//check status recv
+
+                printf(".");
+                sleep(1);
+
+                if(buf > 0)
+                {
+                printf("\nPlayer found\n");//received messages
+                flag = 0;
+                memset(&buf,'\0',sizeof(buf));
+                }
+
+
+            }
+                printf("\n");
+
+
+
+
+
+
+
+            zmq_msg_close (&msg);
+        }
+
+        //free (string);
+        zmq_close(subscriber);
+        zmq_close(publisher);
+        zmq_ctx_shutdown( context ); //optional for cleaning lady order (get ready you l*zy f*ck)
+        zmq_ctx_term( context ); //cleaning lady goes to work
+        return 0;
 }
